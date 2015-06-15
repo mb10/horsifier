81c519d1f1f48ed8f2e0fbc88bd46a69936d3cf3
mb10 <mmbosworth+git@gmail.com>
2015-06-14 21:19:06 -0700

    removed flickr query, added photo list, added button, changed modifications to use mutationobserver

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 var pageMod = require("sdk/page-mod");
 
-pageMod.PageMod({
-  include: "*",
-  contentScriptFile: "./horsify.js"
+var { ToggleButton } = require("sdk/ui/button/toggle");
+
+var button = ToggleButton({
+    id: "my-button1",
+    label: "my button1",
+    icon: "./icon-16.png",
+    onChange: changed,
+    badge: "on",
+    badgeColor: "#00AAAA"
+  });
+
+var mod = pageMod.PageMod({
+	include: "*",
+	contentScriptFile: "./horsify.js",
+	contentScriptWhen: "ready"
 });
+
+function changed(state) {
+	
+  	if (!state.checked) {
+	  	button.badgeColor = "#00AAAA";
+	  	button.badge = "on";
+		mod = pageMod.PageMod({
+			include: "*",
+			contentScriptFile: "./horsify.js",
+			contentScriptWhen: "ready"
+		});
+	}
+  	else {
+    	button.badgeColor = "#AA00AA";
+    	button.badge = "off";
+  		mod.destroy();
+  }
+}
